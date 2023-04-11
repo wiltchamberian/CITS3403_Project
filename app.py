80ec7954b710f18da688eed3805d798eb7a1ae4a
unknown <wiltchamberian@163.com>
2023-04-11 17:15:02 +0800

    test get command, pass

--- app.py
+++ app.py
@@ -4,7 +4,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+    return "Hello World"
+    return render_template('templates/base.html')
+
+#this is for testing
+@app.route('/debug',methods = ['POST', 'GET'])
+def debug():
+    return "Debug"
+    if request.method == 'POST':
+      return "Receive Post"
+    if request.method == 'GET':
+      return "Receive Get"
 
 if __name__ == "__main__":
     app.run(debug=True)
